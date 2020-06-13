098ad48e9033f6633c73ce3a9e20be13bcc2b20e
molBr <pedromollica@gmail.com>
2020-06-13 09:47:12 -0300

    dynamic port enabled

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
   "dunnorobot/cmd/routes"
   "net/http"
   "log"
+  "os"
 )
 
 
 func main() {
+  port := os.Getenv("PORT")
   fmt.Println("Hello world")
   router := routes.CreateRouter()
-  log.Fatal(http.ListenAndServe(":80", router))
+  log.Fatal(http.ListenAndServe(":"+ port , router))
 }
